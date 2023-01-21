d7d6e7b622bd4d3136f0149e230327d0d1a60b72
Fery Setiawan <fery@vectorplay.co.id>
2023-01-21 05:22:08 +0000

    Change the headline & subtitle. Add a textbox and generate buttons.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Summoning: Impure World Reincarnation</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Write a message to Milton Erickson, ask him about anything</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
