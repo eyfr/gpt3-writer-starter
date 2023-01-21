e527de430e052b1151648240be97e4d5019b2278
Fery Setiawan <fery@vectorplay.co.id>
2023-01-21 06:07:13 +0000

    Hook Up with API and add output for UI.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,30 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState('')
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInp: userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput('${output.text}');
+    setIsGenerating(false);
+  }
+ 
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
@@ -14,7 +37,7 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Summoning: Impure World Reincarnation</h1>
+            <h1>Summoning: Impure World Reincarnation (Edo Tensei)</h1>
           </div>
           <div className="header-subtitle">
             <h2>Write a message to Milton Hyland Erickson, ask him about anything</h2>
@@ -29,12 +52,33 @@ const Home = () => {
             onChange={onUserChangedText}
           />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+            <a className="generate-button" onClick{callGenerateEndpoint}>
               <div className="generate">
                 <p>Generate</p>
               </div>
             </a>
           </div>
+          {/* New code I added here */}
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
